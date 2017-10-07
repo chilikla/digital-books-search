faa835bf436c2a357e25d7d22645632dfdf3808f
Chilik Laub HUB-RIGHT <chilik.laub@exlibrisgroup.com>
2017-10-08 02:59:30 +0300

    app.js building

---
+++ app.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('express-session');
+const ejs = require('ejs');
+const express = require('express');
+
+const app = express();
+app.use('/public', express.static('public'));
+app.set('view engine', 'html');
+app.set('views', 'views')
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
