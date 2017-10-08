2f18c28239dbfc796de1e9902b9c17b21072d09c
Chilik Laub HUB-RIGHT <chilik.laub@exlibrisgroup.com>
2017-10-08 03:09:34 +0300

    fixing app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 
 const app = express();
 app.use('/public', express.static('public'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.set('views', 'views')
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
