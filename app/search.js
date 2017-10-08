8dc6aa87de4353df27c5da7a26ba64358e2b986e
Chilik Laub HUB-RIGHT <chilik.laub@exlibrisgroup.com>
2017-10-08 04:04:27 +0300

    fixing bug in search.js

--- app/search.js
+++ app/search.js
@@ -10,8 +10,10 @@ module.exports = function(book, callback) {
     const page = await browser.newPage();
     await page.goto('https://www.mendele.co.il/?s='+encodeURI(book)+'&post_type=product');
     const url = await page.url();
+    conosle.log('final url of the query: '+url);
     if ( url.search('https://www.mendele.co.il/product') > -1 ) {
-      return callback(null, {mendele: {url: url});
+      await browser.close();
+      return callback(null, {mendele: {url: url}});
     }
     else {
       const html = await page.content();
